93daa414dd125c6b7a0d6df077f6fd18db5f9125
Salomon700 <ntwalisolomon9@gmail.com>
2024-11-29 23:47:10 +0200

    task completed

---
+++ 0x06-unitttests_in_js/5-payment.js
@@ -0,0 +1,11 @@
+
+// Basic test using Chai and Sinon Libraries: hooks
+
+const Utils = require('./utils');
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
+};
+
+module.exports = sendPaymentRequestToApi;
